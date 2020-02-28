877faa7bc6cbfe8b0d5c2695adf1826b33ed1b58
Andrea <andrea.antillon.b@gmail.com>
2020-02-28 08:07:11 -0700

    added basic game logic example.

---
+++ Character.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace gun_master_9000
+{
+    class Character
+    {
+
+        public string name;
+
+        public Gun gun;
+
+        //Constructor method, always public and class name.
+        public Character(string name)
+        {
+            this.name = name;
+
+        }
+
+        public void Equip(Gun gun) {
+            this.gun = gun;
+        }
+
+        public void Shoot(Target target) {
+            if (this.gun == null) {
+                throw new System.InvalidOperationException("Character can't shoot without a gun.");
+            }
+            try {
+                this.gun.Shoot(target);
+            } catch (System.InvalidOperationException ex) {
+                Console.WriteLine("*gun clicks*");
+            }
+        }
+    }
+}
---
+++ Target.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace gun_master_9000 {
+    interface Target {
+        void Shot ();
+        
+        bool IsDead();
+    }
+}
